3c56fce8e5d5b78805b5101d810a16e44aabd6b6
Orual <orual.nbc@proton.me>
2025-01-15 14:09:16 -0500

    Update asm.rs

--- xtensa-lx-rt/src/exception/asm.rs
+++ xtensa-lx-rt/src/exception/asm.rs
@@ -1,4 +1,4 @@
-use core::arch::{asm, global_asm};
+use core::arch::{naked_asm, global_asm};
 
 use crate::cfg_asm;
 
@@ -548,7 +548,7 @@ unsafe extern "C" fn __default_naked_exception() {
 #[no_mangle]
 #[link_section = ".rwtext"]
 unsafe extern "C" fn __default_naked_double_exception() {
-    asm!(
+    naked_asm!(
         "
         mov     a0, a1                     // save a1/sp
         addmi   sp, sp, -XT_STK_FRMSZ      // only allow multiple of 256
@@ -626,7 +626,7 @@ global_asm!(
 #[no_mangle]
 #[link_section = ".rwtext"]
 unsafe extern "C" fn __default_naked_level_2_interrupt() {
-    asm!("HANDLE_INTERRUPT_LEVEL 2", options(noreturn));
+    naked_asm!("HANDLE_INTERRUPT_LEVEL 2", options(noreturn));
 }
 
 /// Handle Level 3 Interrupt by storing full context and then calling regular
@@ -638,7 +638,7 @@ unsafe extern "C" fn __default_naked_level_2_interrupt() {
 #[no_mangle]
 #[link_section = ".rwtext"]
 unsafe extern "C" fn __default_naked_level_3_interrupt() {
-    asm!("HANDLE_INTERRUPT_LEVEL 3", options(noreturn));
+    naked_asm!("HANDLE_INTERRUPT_LEVEL 3", options(noreturn));
 }
 
 /// Handle Level 4 Interrupt by storing full context and then calling regular
@@ -650,7 +650,7 @@ unsafe extern "C" fn __default_naked_level_3_interrupt() {
 #[no_mangle]
 #[link_section = ".rwtext"]
 unsafe extern "C" fn __default_naked_level_4_interrupt() {
-    asm!("HANDLE_INTERRUPT_LEVEL 4", options(noreturn));
+    naked_asm!("HANDLE_INTERRUPT_LEVEL 4", options(noreturn));
 }
 
 /// Handle Level 5 Interrupt by storing full context and then calling regular
@@ -662,7 +662,7 @@ unsafe extern "C" fn __default_naked_level_4_interrupt() {
 #[no_mangle]
 #[link_section = ".rwtext"]
 unsafe extern "C" fn __default_naked_level_5_interrupt() {
-    asm!("HANDLE_INTERRUPT_LEVEL 5", options(noreturn));
+    naked_asm!("HANDLE_INTERRUPT_LEVEL 5", options(noreturn));
 }
 
 /// Handle Level 6 (=Debug) Interrupt by storing full context and then calling
@@ -674,7 +674,7 @@ unsafe extern "C" fn __default_naked_level_5_interrupt() {
 #[no_mangle]
 #[link_section = ".rwtext"]
 unsafe extern "C" fn __default_naked_level_6_interrupt() {
-    asm!("HANDLE_INTERRUPT_LEVEL 6", options(noreturn));
+    naked_asm!("HANDLE_INTERRUPT_LEVEL 6", options(noreturn));
 }
 
 /// Handle Level 7 (=NMI) Interrupt by storing full context and then calling
@@ -686,5 +686,5 @@ unsafe extern "C" fn __default_naked_level_6_interrupt() {
 #[no_mangle]
 #[link_section = ".rwtext"]
 unsafe extern "C" fn __default_naked_level_7_interrupt() {
-    asm!("HANDLE_INTERRUPT_LEVEL 7", options(noreturn));
+    naked_asm!("HANDLE_INTERRUPT_LEVEL 7", options(noreturn));
 }
